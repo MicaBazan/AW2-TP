e7db03ad8fc44649904e8e2bd4859e983223fc9e
MicaBazan <micabazan5@gmail.com>
2024-06-12 23:49:06 -0300

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ import express from 'express'
 import categoriaRouter from './routes/categorias.routes.js'
 import productoRouter from './routes/productos.routes.js'
 import usuariosRouter from './routes/usuarios.routes.js'
+import ordenesRouter from './routes/ordenes.routes.js'
 
 const app = express()
 
 app.use(express.json())
+
+const port = 3000
+
+app.listen(port, ()=>{
+    console.log(`servidor levantado en el puerto ${port}`)
+})
+
+app.use('/categorias', categoriaRouter)
+app.use('./productos', productoRouter)
+app.use('./usuarios', usuariosRouter)
+app.use('./ordenes', ordenesRouter)
