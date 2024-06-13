58512f08d28f8028362ee3e363178060b38f26d0
MicaBazan <micabazan5@gmail.com>
2024-06-13 00:43:18 -0300

    modificarUsuario

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,5 +1,5 @@
 import { error } from "console"
-import { Router } from "express"
+import e, { Router } from "express"
 import { readFile, writeFile } from 'fs/promises'
 
 const fileUsuarios = await readFile('./data/usuarios.json', 'utf-8')
@@ -53,10 +53,48 @@ router.delete('/eliminarUsuario/:id', (req, res) =>{
     if(usuarioIndice === -1){
         return res.status(400).json({ error: 'Usuario no encontrado' })
     }
-    
+
+    //falta terminar
 })
 
+router.put('/modificarUsuario/:id', (req, res) => {
+    const id = req.params
+    const usuario = req.body.usuario
+    const nombre = req.body.nombre
+    const apellido = req.body.apellido
+    const pass = req.body.pass
+    const email = req.body.email
+    const telefono = req.body.telefono
 
+    const usuarioIndex = usuariosData.findIndex(user => user.id == id)
+
+    if (usuarioIndex === -1) {
+        return res.status(404).json({ error: 'Usuario no encontrado.' })
+    }
+
+    const usuarioExistente = usuariosData.find(e => 
+        (e.usuario === usuario || e.email === email) && e.id != id
+    )
+
+    if (usuarioExistente) {
+        return res.status(400).json({ error: 'El nombre de usuario o el email ya existe.' })
+    }
+
+    // Modificar los campos del usuario
+    if (usuario) usuariosData[usuarioIndex].usuario = usuario;
+    if (nombre) usuariosData[usuarioIndex].nombre = nombre;
+    if (apellido) usuariosData[usuarioIndex].apellido = apellido;
+    if (pass) usuariosData[usuarioIndex].pass = pass;
+    if (email) usuariosData[usuarioIndex].email = email;
+    if (telefono) usuariosData[usuarioIndex].telefono = telefono;
+
+    try {
+        fs.writeFileSync('./data/usuarios.json', JSON.stringify(usuariosData, null, 2));
+        res.status(200).json(usuariosData[usuarioIndex]);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al modificar el usuario.' });
+    }
+})
 
 
 export default router
